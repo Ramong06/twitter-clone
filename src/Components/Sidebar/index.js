d471c8452820afaec7707b220451a42dc1663ec7
RG-Mac <ramong5406@gmail.com>
2021-01-01 16:37:22 -0600

    created sidebar option component

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./style.css";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import SidebarOption from "../SidebarOption";
 
 const Sidebar = () => {
     return (
@@ -8,6 +9,7 @@ const Sidebar = () => {
             {/* Twitter icon */}
             <TwitterIcon />
             {/* SidebarOption */}
+            <SidebarOption />
             {/* SidebarOption */}
             {/* SidebarOption */}
             {/* SidebarOption */}
---
+++ src/Components/SidebarOption/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const SidebarOption = () => {
+    return (
+        <div className="sidebarOption">
+            <h2>This is a sidebar option</h2>
+        </div>
+    );
+}
+
+export default SidebarOption;
