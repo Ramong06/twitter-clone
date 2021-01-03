f2444bf3f5c5f15bdf90efc11457f1f91e26c30e
RG-Mac <ramong5406@gmail.com>
2021-01-03 16:59:01 -0600

    built out Feed component outline

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Sidebar from "./Components/Sidebar";
+import Feed from "./Components/Feed";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Sidebar />
       
       {/* feed */}
+      <Feed />
 
       {/* widgets */}
 
--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -3,7 +3,25 @@ import React from 'react'
 const Feed = () => {
     return (
         <div>
-            
+            {/* HEADER */}
+            <h2>This is the Feed Header</h2>
+
+            {/* TWEETBOX */}
+            <h4>This is where you write your tweet</h4>
+
+            {/* POST */}
+            <h5>Tweet</h5>
+            {/* POST */}
+            <h5>Tweet</h5>
+            {/* POST */}
+            <h5>Tweet</h5>
+            {/* POST */}
+            <h5>Tweet</h5>
+            {/* POST */}
+            <h5>Tweet</h5>
+            {/* POST */}
+            <h5>Tweet</h5>
+
         </div>
     )
 }
