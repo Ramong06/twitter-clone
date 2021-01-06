667560e0d2635319778fa3e71e794a9b840d119c
RG-Mac <ramong5406@gmail.com>
2021-01-06 11:52:28 -0600

    finished styling tweetBox, added image input

--- src/Components/TweetBox/index.js
+++ src/Components/TweetBox/index.js
@@ -9,8 +9,8 @@ const TweetBox = () => {
                 <div className="tweetBox__input">
                     <Avatar src="https://i.pinimg.com/originals/9d/e4/94/9de49481e0c866734d1362ea6f1bb608.jpg" />
                     <input placeholder="What's Happening?" type="text" />
-                    {/* <input placeholder="Enter Image URL" type="text" /> */}
                 </div>
+                <input className="tweetBox__imageInput" placeholder="Enter Image URL" type="text" />
                 <Button className="tweetBox__button">Tweet</Button>
             </form>
         </div>
