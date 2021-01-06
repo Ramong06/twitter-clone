a8178d8acff50c7ce95088163d526823fc4b3668
RG-Mac <ramong5406@gmail.com>
2021-01-06 10:59:36 -0600

    moved tweet button outside div in tweetbox

--- src/Components/TweetBox/index.js
+++ src/Components/TweetBox/index.js
@@ -9,8 +9,9 @@ const TweetBox = () => {
                 <div className="tweetBox__input">
                     <Avatar src="https://i.pinimg.com/originals/9d/e4/94/9de49481e0c866734d1362ea6f1bb608.jpg" />
                     <input placeholder="What's Happening?" type="text" />
-                    <Button>Tweet</Button>
+                    {/* <input placeholder="Enter Image URL" type="text" /> */}
                 </div>
+                <Button>Tweet</Button>
             </form>
         </div>
     )
