7945d71da501ab88487d3391a65f2cdc8318b15e
RG-Mac <ramong5406@gmail.com>
2021-01-06 17:44:48 -0600

    added icons to post for message, retweet, heart and upload

--- src/Components/Post/index.js
+++ src/Components/Post/index.js
@@ -2,6 +2,10 @@ import { Avatar } from '@material-ui/core';
 import React from 'react'
 import "./style.css";
 import VerifiedUserIcon from "@material-ui/icons/VerifiedUser"
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import RepeatIcon from "@material-ui/icons/RepeatOneOutlined";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorderOutlined";
+import PublishIcon from "@material-ui/icons/PublishOutlined";
 
 const Post = ({ displayName, username, verified, text, image, avatar }) => {
     return (
@@ -24,6 +28,12 @@ const Post = ({ displayName, username, verified, text, image, avatar }) => {
                     </div>
                 </div>
                 <img src="https://i.pinimg.com/originals/05/3c/39/053c39f4c14d99d4bc143c05dc3ca219.gif" alt="super-mario" />
+                <div className="post__footer">
+                    <ChatBubbleOutlineIcon fontSize="small" />
+                    <RepeatIcon fontSize="small" />
+                    <FavoriteBorderIcon fontSize="small" />
+                    <PublishIcon fontSize="small" />
+                </div>
             </div>
         </div>
     )
