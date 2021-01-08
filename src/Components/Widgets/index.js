2a7dc0416bff5687355f23371169bc696ef183cf
RG-Mac <ramong5406@gmail.com>
2021-01-08 16:33:53 -0600

    installed TwitterEmbed, added tweets, timeline and share button

--- src/Components/Widgets/index.js
+++ src/Components/Widgets/index.js
@@ -1,11 +1,24 @@
 import React from 'react'
+import "./style.css";
+import { TwitterTimelineEmbed, TwitterShareButton, TwitterTweetEmbed } from "react-twitter-embed";
+import SearchIcon from "@material-ui/icons/SearchOutlined";
+
 
 const Widgets = () => {
     return (
         <div className="widgets">
-            <h2>Widgets</h2>
+            <div className="widgets__input">
+                <SearchIcon className="widgets__searchIcon" />
+                <input placeholder="Search Twitter" type="text" />
+            </div>
+            <div className="widgets__widgetsContainer">
+                <h2>What's Happening</h2>
+                <TwitterTweetEmbed tweetId={"1347049835246071809"} />
+                <TwitterTimelineEmbed sourceType="profile" screenName="rgtercero5406" options={{ height: 400 }} />
+                <TwitterShareButton url={"https://www.facebook.com/ramon.garzaiii"} options={{ text: "#reactjs is lit!", via: "ramon.garzaiii" }} />
+            </div>
         </div>
     )
 }
 
-export default Widgets 
+export default Widgets;
