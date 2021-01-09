21420afd229af428a760bb04dff440cfd7b99fa6
RG-Mac <ramong5406@gmail.com>
2021-01-09 02:06:44 -0600

    fixed className in widget, styled widgets__widgetsContainer

--- src/Components/Widgets/index.js
+++ src/Components/Widgets/index.js
@@ -13,7 +13,7 @@ const Widgets = () => {
             </div>
             <div className="widgets__widgetsContainer">
                 <h2>What's Happening</h2>
-                <TwitterTweetEmbed tweetId={"1347049835246071809"} />
+                <TwitterTweetEmbed tweetId={"1331064325449125888"} />
                 <TwitterTimelineEmbed sourceType="profile" screenName="rgtercero5406" options={{ height: 400 }} />
                 <TwitterShareButton url={"https://www.facebook.com/ramon.garzaiii"} options={{ text: "#reactjs is lit!", via: "ramon.garzaiii" }} />
             </div>
