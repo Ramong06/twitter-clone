66d28b398d07aa6b669de8423b705b32a47090ef
RG-Mac <ramong5406@gmail.com>
2021-01-09 19:35:00 -0600

    successfully connected firebase database, now pulling post info from db

--- src/Components/Feed/index.js
+++ src/Components/Feed/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./style.css";
 import TweetBox from "../TweetBox";
 import Post from "../Post";
+import db from "../../firebase";
 
 const Feed = () => {
+    const [posts, setPosts] = useState([]);
+
+      useEffect(() => {
+          db.collection("posts").onSnapshot(snapshot => (
+            setPosts(snapshot.docs.map(doc => doc.data()))
+        ))
+  }, []);
+
     return (
         <div className="feed">
             {/* HEADER */}
@@ -16,10 +25,17 @@ const Feed = () => {
 
 
             {/* POST */}
-            <Post displayName="Ramon Garza" username="rgtercero5406" verified={true} text="This looks like it's working!" avatar="https://i.pinimg.com/originals/9d/e4/94/9de49481e0c866734d1362ea6f1bb608.jpg" image="https://i.pinimg.com/originals/05/3c/39/053c39f4c14d99d4bc143c05dc3ca219.gif" />
-            
-            
-            
+            {posts.map(post => (
+                < Post
+                    displayName={post.displayName}
+                    username={post.username}
+                    verified={post.verified}
+                    text={post.text}
+                    avatar={post.avatar}
+                    image={post.image}
+                />
+            ))}
+             
         </div>
     )
 }
